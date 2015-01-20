6e9d2577b7d5ea489966b61d1e830ee3d6c9996b
Thomas Portelange <thomas.portelange@boomerang.be>
2015-01-20 18:24:46 +0100

    code is not running properly, just run in the command line

--- code/tasks/DeployTask.php
+++ code/tasks/DeployTask.php
@@ -27,34 +27,52 @@ class DeployTask extends BuildTask
         }
 
         if ($target == 'staging') {
-            $port   = DEPLOY_STAGING_PORT;
+            if (!defined('DEPLOY_STAGING_PORT') || !defined('DEPLOY_STAGING_TARGET')) {
+                $this->out('Missing constant. Please define in _ss_environment');
+                exit();
+            }
+            $port      = DEPLOY_STAGING_PORT;
             $sshtarget = DEPLOY_STAGING_TARGET;
         } else if ($target == 'live') {
-            $port   = DEPLOY_LIVE_PORT;
+            if (!defined('DEPLOY_LIVE_PORT') || !defined('DEPLOY_LIVE_TARGET')) {
+                $this->out('Missing constant. Please define in _ss_environment');
+                exit();
+            }
+            $port      = DEPLOY_LIVE_PORT;
             $sshtarget = DEPLOY_LIVE_TARGET;
         } else {
             $this->out('Invalid target');
             exit();
         }
 
+        $hosts = exec('cat ~/.ssh/known_hosts');
+        if(!strlen($hosts)) {
+            $this->out('No known hosts to deploy to');
+            exit();
+        }
+
+//        $this->out(shell_exec('ssh-add -l 2>&1'));
+        
+        $dry = '';
         if ($go) {
             $this->out('Executing actual deploy on '.$target);
         } else {
+            $dry = ' --dry-run';
             $this->out('Executing dry run on '.$target);
         }
 
         $this->out('Running script as '.exec('whoami'));
 
-
-        $output = '';
-        chdir(Director::baseFolder());
-        $script = 'rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p'.$port.'" ./ '.$sshtarget.' 2>&1';
+//        $output = '';
+//        chdir(Director::baseFolder());
+        $script = 'rsync'.$dry.' -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -v -p'.$port.'" ./ '.$sshtarget.' 2>&1';
         $this->out($script);
-        exec($script, $output);
-        foreach ($output as $line) {
-            $this->out($line);
-        }
-        return $output;
+//        exec($script, $output);
+//        foreach ($output as $line) {
+//            $this->out($line);
+//        }
+
+        $this->out('Run the above line in the command line');
     }
 
     public function out($message)
