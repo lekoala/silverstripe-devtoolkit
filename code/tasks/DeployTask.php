a47d1aa4f06e3f6a0d07248b9b7f4560475ce920
Thomas Portelange <thomas.portelange@boomerang.be>
2015-01-20 17:25:00 +0100

    sample deploy code

--- code/tasks/DeployTask.php
+++ code/tasks/DeployTask.php
@@ -12,6 +12,14 @@ class DeployTask extends BuildTask
 
     public function run($request)
     {
-        Debug::message('No target!');
+        $target = $request->getVar('target');
+        $go = $request->getVar('go') ? true : false;
+        if(!$target) {
+            return 'You must pass a target, either ?target=live or ?target=staging';
+        }
+        $output = '';
+        exec('rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p121" ./../../../ rooot@vps128787.ovh.net:/websites/dgsport.eu', $output);
+
+        return $output;
     }
 }
