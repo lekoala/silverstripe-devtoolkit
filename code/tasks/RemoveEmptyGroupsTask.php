07f48630b37cb4127ab480c042e4c15659e319dc
Thomas <thomas@lekoala.be>
2015-09-24 13:22:49 +0200

    update task to drop groups without permissions

--- code/tasks/RemoveEmptyGroupsTask.php
+++ code/tasks/RemoveEmptyGroupsTask.php
@@ -5,15 +5,26 @@
  */
 class RemoveEmptyGroupsTask extends BuildTask
 {
-    protected $title       = "Remove empty groups from the cms";
+    protected $title = "Remove empty groups from the cms";
 
     public function run($request)
     {
-        $groups = Group::get();
-        foreach($groups as $group) {
-            if(!$group->Members()->count()) {
-                DB::alteration_message("Removing group {$group->ID}","deleted");
+        $groups           = Group::get();
+        echo 'Want more dropping? Pass ?permission=1 to also drop groups without permissions even if they have members';
+        echo '<hr/>';
+        $dropNoPermission = $request->getVar('permission');
+        foreach ($groups as $group) {
+            if (!$group->Members()->count()) {
+                DB::alteration_message("Removing group  {$group->ID} because it has no members", "deleted");
                 $group->delete();
+            }
+            if ($dropNoPermission) {
+                $c = $group->Permissions()->count();
+                if (!$c) {
+                    DB::alteration_message("Removing group {$group->ID} because it has no permissions",
+                        "deleted");
+                    $group->delete();
+                }
             }
         }
         DB::alteration_message('All done!');
