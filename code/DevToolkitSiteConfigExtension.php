63c4f02fd80a1866e2369e2fc07878a38a7de698
Thomas <thomas@lekoala.be>
2015-10-06 17:02:58 +0200

    add local helper

--- code/DevToolkitSiteConfigExtension.php
+++ code/DevToolkitSiteConfigExtension.php
@@ -8,6 +8,22 @@
 class DevToolkitSiteConfigExtension extends DataExtension
 {
 
+    public static function isLocal()
+    {
+        if (empty($_SERVER['REMOTE_ADDR'])) {
+            return;
+        }
+
+        $whitelist = array(
+            '127.0.0.1',
+            '::1'
+        );
+        if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+            return true;
+        }
+        return false;
+    }
+
     public function updateCMSFields(\FieldList $fields)
     {
         if (defined('SS_ENVIRONMENT_FILE') && Permission::check('ADMIN')) {
@@ -21,16 +37,16 @@ class DevToolkitSiteConfigExtension extends DataExtension
             if (class_exists('CodeEditorField')) {
                 $field->setMode('php');
             }
-            if(!is_writable(SS_ENVIRONMENT_FILE)) {
+            if (!is_writable(SS_ENVIRONMENT_FILE)) {
                 $field->setReadonly(true);
             }
         }
     }
 
-      public function setSS_Environment($v)
+    public function setSS_Environment($v)
     {
         if (!is_writable(SS_ENVIRONMENT_FILE)) {
-			throw new Exception('Environment file must be writable');
+            throw new Exception('Environment file must be writable');
         } else {
             return file_put_contents(SS_ENVIRONMENT_FILE, $v);
         }
