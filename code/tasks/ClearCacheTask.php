7e3a44154d5062f03067c8170518d232d06f4a04
Thomas <thomas@lekoala.be>
2016-08-30 15:03:25 +0200

    allow flush memcache

--- code/tasks/ClearCacheTask.php
+++ code/tasks/ClearCacheTask.php
@@ -12,15 +12,28 @@ class ClearCacheTask extends BuildTask
 
     public function run($request)
     {
-        $folder = TEMP_FOLDER;
+        // Flush memcache
+        if (defined('DEVTOOLKIT_USE_MEMCACHED') && DEVTOOLKIT_USE_MEMCACHED) {
+            $host = defined('MEMCACHE_HOST') ? MEMCACHE_HOST : 'localhost';
+            $port = defined('MEMCACHE_PORT') ? MEMCACHE_PORT : 11211;
 
+            $memcache  = new Memcache;
+            $connected = $memcache->connect($host, $port);
+            if($connected) {
+                echo "Flush memcache<br/>";
+                $memcache->flush();
+            }
+        }
+
+        // Clear file cache
+        $folder = TEMP_FOLDER;
 
         $di = new RecursiveDirectoryIterator($folder,
             FilesystemIterator::SKIP_DOTS);
         $ri = new RecursiveIteratorIterator($di,
             RecursiveIteratorIterator::CHILD_FIRST);
         foreach ($ri as $file) {
-            echo "Deleting " . $file . '<br/>';
+            echo "Deleting ".$file.'<br/>';
             if ($file->isFile()) {
                 unlink($file);
             }
