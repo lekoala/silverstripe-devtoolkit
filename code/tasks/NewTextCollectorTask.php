69bba967129ae8355ae4931ef32d29f680ed8f05
Thomas <thomas@lekoala.be>
2017-10-18 15:43:03 +0200

    allow modules with no _config.php

--- code/tasks/NewTextCollectorTask.php
+++ code/tasks/NewTextCollectorTask.php
@@ -1,5 +1,4 @@
 <?php
-
 class Newi18nTextCollector extends i18nTextCollector
 {
     protected $clearUnused = false;
@@ -16,16 +15,21 @@ class Newi18nTextCollector extends i18nTextCollector
 
     public function run($restrictToModules = null, $mergeWithExisting = false)
     {
-        $entitiesByModule = $this->collect($restrictToModules,
-            $mergeWithExisting);
+        $entitiesByModule = $this->collect(
+            $restrictToModules,
+            $mergeWithExisting
+        );
 
         $results = array();
 
         // Write each module language file
         if ($entitiesByModule) {
             foreach ($entitiesByModule as $module => $entities) {
-                $results[$module] = $this->getWriter()->write($entities,
-                    $this->defaultLocale, $this->baseSavePath.'/'.$module);
+                $results[$module] = $this->getWriter()->write(
+                    $entities,
+                    $this->defaultLocale,
+                    $this->baseSavePath . '/' . $module
+                );
             }
         }
 
@@ -36,7 +40,7 @@ class Newi18nTextCollector extends i18nTextCollector
     {
         $clearUnused = $this->getClearUnused();
 
-        $glob    = glob($this->basePath.'/*', GLOB_ONLYDIR);
+        $glob = glob($this->basePath . '/*', GLOB_ONLYDIR);
         $modules = array_map(function ($item) {
             return basename($item);
         }, $glob);
@@ -50,13 +54,17 @@ class Newi18nTextCollector extends i18nTextCollector
         foreach ($modules as $index => $module) {
             if ($module != 'themes') {
                 continue;
-            } else {
-                $themes = scandir($this->basePath."/themes");
+            }
+            else {
+                $themes = scandir($this->basePath . "/themes");
                 if (count($themes)) {
                     foreach ($themes as $theme) {
-                        if (is_dir($this->basePath."/themes/".$theme) && substr($theme,
-                                0, 1) != '.' && is_dir($this->basePath."/themes/".$theme."/templates")) {
-                            $themeFolders[] = 'themes/'.$theme;
+                        if (is_dir($this->basePath . "/themes/" . $theme) && substr(
+                            $theme,
+                            0,
+                            1
+                        ) != '.' && is_dir($this->basePath . "/themes/" . $theme . "/templates")) {
+                            $themeFolders[] = 'themes/' . $theme;
                         }
                     }
                 }
@@ -77,12 +85,9 @@ class Newi18nTextCollector extends i18nTextCollector
 
             // Only search for calls in folder with a _config.php file (which means they are modules, including
             // themes folder)
-            $isValidModuleFolder = (
-                is_dir("$this->basePath/$module") && is_file("$this->basePath/$module/_config.php")
-                && substr($module, 0, 1) != '.'
-                ) || (
-                substr($module, 0, 7) == 'themes/' && is_dir("$this->basePath/$module")
-                );
+            $isValidModuleFolder = (is_dir("$this->basePath/$module")
+                && (is_file("$this->basePath/$module/_config.php") || (is_dir("$this->basePath/$module/_config")))
+                && (substr($module, 0, 1) != '.') || (substr($module, 0, 7) == 'themes/' && is_dir("$this->basePath/$module")));
 
             if (!$isValidModuleFolder) {
                 continue;
@@ -92,18 +97,21 @@ class Newi18nTextCollector extends i18nTextCollector
             $processedEntities = $this->processModule($module);
 
             if (isset($entitiesByModule[$module])) {
-                $entitiesByModule[$module] = array_merge_recursive($entitiesByModule[$module],
-                    $processedEntities);
-            } else {
+                $entitiesByModule[$module] = array_merge_recursive(
+                    $entitiesByModule[$module],
+                    $processedEntities
+                );
+            }
+            else {
                 $entitiesByModule[$module] = $processedEntities;
             }
 
             // extract all entities for "foreign" modules (fourth argument)
             foreach ($entitiesByModule[$module] as $fullName => $spec) {
                 if (isset($spec[2]) && $spec[2] && $spec[2] != $module) {
-                    $othermodule                               = $spec[2];
+                    $othermodule = $spec[2];
                     if (!isset($entitiesByModule[$othermodule])) {
-                        $entitiesByModule[$othermodule]            = array();
+                        $entitiesByModule[$othermodule] = array();
                     }
                     unset($spec[2]);
                     $entitiesByModule[$othermodule][$fullName] = $spec;
@@ -123,13 +131,15 @@ class Newi18nTextCollector extends i18nTextCollector
                                 $locale = $fluentLocale;
                             }
                         }
-                    } else {
+                    }
+                    else {
                         $locale = i18n::get_locale_from_lang($locale);
                     }
                 }
-                $adapter    = Injector::inst()->create('i18nRailsYamlAdapter');
+                $adapter = @Injector::inst()->create('i18nRailsYamlAdapter');
                 $masterFile = "{$this->basePath}/{$module}/lang/"
-                    .$adapter->getFilenameForLocale($this->defaultLocale);
+                    . $adapter->getFilenameForLocale($this->defaultLocale);
+
                 if (!file_exists($masterFile)) {
                     continue;
                 }
@@ -144,19 +154,20 @@ class Newi18nTextCollector extends i18nTextCollector
                 //do not overwrite by interverting
                 $messages = array_map(
                     // Transform each master string from scalar value to array of strings
-                    function ($v) {
+function ($v) {
                     return array($v);
-                }, $adapter->getMessages($this->defaultLocale)
-                );
+                }, $adapter->getMessages($this->defaultLocale));
 
                 $entitiesByModule[$module] = array_merge(
-                    $entitiesByModule[$module], $messages
+                    $entitiesByModule[$module],
+                    $messages
                 );
 
                 //clear by diffing
 
                 if ($clearUnused) {
-                    $unusedEntities = array_diff(array_keys($messages),
+                    $unusedEntities = array_diff(
+                        array_keys($messages),
                         array_keys($processedEntities)
                     );
 
@@ -177,7 +188,7 @@ class Newi18nTextCollector extends i18nTextCollector
                             continue;
                         }
 
-                        echo '<div style="color:red">Removed translations for '.$unusedEntity.'</div>';
+                        echo '<div style="color:red">Removed translations for ' . $unusedEntity . '</div>';
                         unset($entitiesByModule[$module][$unusedEntity]);
                     }
                 }
@@ -186,8 +197,10 @@ class Newi18nTextCollector extends i18nTextCollector
 
         // Restrict modules we update to just the specified ones (if any passed)
         if ($restrictToModules && count($restrictToModules)) {
-            foreach (array_diff(array_keys($entitiesByModule),
-                $restrictToModules) as $module) {
+            foreach (array_diff(
+                array_keys($entitiesByModule),
+                $restrictToModules
+            ) as $module) {
                 unset($entitiesByModule[$module]);
             }
         }
@@ -203,7 +216,7 @@ class Newi18nTextCollector extends i18nTextCollector
  */
 class NewTextCollectorTask extends i18nTextCollectorTask
 {
-    protected $title       = "i18n Textcollector Task (improved)";
+    protected $title = "i18n Textcollector Task (improved)";
     protected $description = 'Create or update translation files';
 
     public function run($request)
@@ -227,8 +240,10 @@ class NewTextCollectorTask extends i18nTextCollectorTask
         }
 
         //Scope to mysite by default
-        $restrictModules = ($request->getVar('module')) ? explode(',',
-                $request->getVar('module')) : array('mysite');
+        $restrictModules = ($request->getVar('module')) ? explode(
+            ',',
+            $request->getVar('module')
+        ) : array('mysite');
 
 
         echo 'You can pass ?module=mymodule,myothermodule to restrict to a specific module list<br/>';
@@ -237,7 +252,7 @@ class NewTextCollectorTask extends i18nTextCollectorTask
         echo 'You can clear unused translations by specifying ?clear=1<br/>';
         echo '<hr/>';
 
-        echo 'Collecting text for '.implode(',', $restrictModules).' and locale '.$locale;
+        echo 'Collecting text for ' . implode(',', $restrictModules) . ' and locale ' . $locale;
         echo '<hr/>';
 
         $result = $c->run($restrictModules, true);
@@ -249,7 +264,8 @@ class NewTextCollectorTask extends i18nTextCollectorTask
         foreach ($result as $module => $res) {
             if ($res) {
                 echo "<div style='color:green'>Collected text from $module</div>";
-            } else {
+            }
+            else {
                 echo "<div style='color:red'>Failed to collect text from $module</div>";
             }
         }
