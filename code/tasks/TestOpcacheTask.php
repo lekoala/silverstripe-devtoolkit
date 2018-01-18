67016f0e39fdfd55d80e7e191ffcb924e7addecc
Thomas <thomas@lekoala.be>
2018-01-18 12:18:03 +0100

    opcache test task

---
+++ code/tasks/TestOpcacheTask.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Description of TestOpcacheTask
+ *
+ * @author Koala
+ */
+class TestOpcacheTask extends BuildTask
+{
+    protected $title = "Test Opache";
+
+    public function run($request)
+    {
+        if (!function_exists('opcache_get_status')) {
+            $this->msg("opcache_get_status function is not defined");
+        }
+
+        $result = opcache_get_status();
+        if ($result) {
+            $this->msg("Opcache is active");
+
+            echo '<pre>';
+            print_r($result);
+            echo '</pre>';
+        } else {
+            $this->msg("Opcache is disabled. It should be enabled to ensure optimal performances", "error");
+        }
+    }
+
+    protected function msg($msg)
+    {
+        DB::alteration_message($msg);
+    }
+}
