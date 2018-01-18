d6c5c510aa467414f6a2f2d60eda6431ea1489ac
Thomas <thomas@lekoala.be>
2018-01-18 12:02:04 +0100

    configurable password validators

---
+++ code/validators/DefaultPasswordValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * DefaultPasswordValidator
+ * 
+ * A configurable password validator
+ *
+ * @author lekoala
+ */
+class DefaultPasswordValidator extends PasswordValidator
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $config = self::config();
+        if ($config->min_length) {
+            $this->minLength($config->min_length);
+        }
+        if ($config->historical_passwords) {
+            $this->checkHistoricalPasswords($config->historical_passwords);
+        }
+        $characters = array_unique($config->characters);
+        $count = $config->characters_count;
+        if (!empty($characters)) {
+            if (!$count) {
+                $count = count($characters);
+            }
+            $this->characterStrength($count, $characters);
+        }
+    }
+
+    public static function applyToMember()
+    {
+        $class = get_called_class();
+        Member::set_password_validator($class);
+    }
+}
